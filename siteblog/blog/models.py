f47a8955286d8913262f3d9a113aa22dffca884e
Danila Berazhny <dannmmaster@gmail.com>
2021-09-17 19:08:53 +0300

    Initial commit

---
+++ siteblog/blog/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"slug": self.slug})
+    
+
+    class Meta:
+        ordering = ['title']
+        verbose_name_plural = 'Categories'
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("tag", kwargs={"slug": self.slug})
+    
+    class Meta:
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.CharField(max_length=100)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    views = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-created_at']
+    
+    def get_absolute_url(self):
+        return reverse("post", kwargs={"slug": self.slug})
+
